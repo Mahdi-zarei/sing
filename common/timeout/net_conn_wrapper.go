3f68b574643e190b70441c988efa7529818122cd
unknown <mahdi.zrei@gmail.com>
2023-12-10 03:25:20 +0330

    implement deadline wrapper

---
+++ common/timeout/net_conn_wrapper.go
@@ -0,0 +1,57 @@
+package timeout
+
+import (
+	"net"
+	"time"
+)
+
+type NetConnWithTimeout struct {
+	net.Conn
+	timeout        time.Duration
+	lastSet        time.Time
+	manualDeadline time.Time
+}
+
+func NewNetConnWithTimeout(conn net.Conn, timeout time.Duration) net.Conn {
+	return &NetConnWithTimeout{
+		Conn:    conn,
+		timeout: timeout,
+	}
+}
+
+func (c *NetConnWithTimeout) Read(b []byte) (n int, err error) {
+	if time.Since(c.lastSet) >= 10*time.Second && time.Now().After(c.manualDeadline) {
+		_ = c.Conn.SetDeadline(time.Now().Add(c.timeout))
+		c.lastSet = time.Now()
+	}
+
+	return c.Conn.Read(b)
+}
+
+func (c *NetConnWithTimeout) Write(b []byte) (n int, err error) {
+	if time.Since(c.lastSet) >= 10*time.Second && time.Now().After(c.manualDeadline) {
+		_ = c.Conn.SetDeadline(time.Now().Add(c.timeout))
+		c.lastSet = time.Now()
+	}
+
+	return c.Conn.Write(b)
+}
+
+func (c *NetConnWithTimeout) SetReadDeadline(t time.Time) error {
+	c.manualDeadline = t
+	return c.Conn.SetReadDeadline(t)
+}
+
+func (c *NetConnWithTimeout) SetWriteDeadline(t time.Time) error {
+	c.manualDeadline = t
+	return c.Conn.SetWriteDeadline(t)
+}
+
+func (c *NetConnWithTimeout) SetDeadline(t time.Time) error {
+	c.manualDeadline = t
+	return c.Conn.SetDeadline(t)
+}
+
+func (c *NetConnWithTimeout) Upstream() any {
+	return c.Conn
+}
